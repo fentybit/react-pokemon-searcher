4a9d12691a8426a48eadb4ffc8bbc18db2a57ef3
fentybit <fentymhall@gmail.com>
2021-04-18 21:47:35 -0700

    finish lab

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,49 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemons: [],
+    searchVal: ''
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pokemon')
+      .then(resp => resp.json())
+      .then(pokemons => this.setState({ pokemons: pokemons }))
+  }
+
+  addPokemon = (newPokemon) => {
+    this.setState({ pokemons: [...this.state.pokemons, newPokemon] })
+  }
+
+  handleOnChange = (event) => {
+    this.setState({ searchVal: event.target.value })
+  }
+
+  toggleImage = pokemon => {
+    const { pokemons } = this.state
+    const index = pokemons.indexOf(pokemon)
+    this.setState({
+      pokemons: [
+        ...pokemons.slice(0, index),
+        { ...pokemon, isClicked: !pokemon.isClicked },
+        ...pokemons.slice(index + 1)
+      ]
+    })
+  }
+
   render() {
+    const filteredPokemons = this.state.pokemons.filter(poke => poke.name.includes(this.state.searchVal))
+
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon={this.addPokemon} />
         <br />
-        <Search />
+        <Search onChange={this.handleOnChange} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={filteredPokemons} toggleImage={this.toggleImage} />
       </Container>
     )
   }

4ecf65bce2c628b1f7cb94b2c3648b0658d6596e
fentybit <fentymhall@gmail.com>
2021-04-18 21:46:24 -0700

    commit tree 02943dc2d06c5bf91daf8b2611f4f4842c025a9b

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,24 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+
   render() {
+    const url = this.props.pokemon.isClicked ? this.props.pokemon.sprites.back : this.props.pokemon.sprites.front
+
     return (
       <Card>
-        <div>
+        <div onClick={() => this.props.toggleImage(this.props.pokemon)}>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={url} alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.props.pokemon.hp}
             </span>
           </div>
         </div>
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -2,16 +2,48 @@ import React from 'react'
 import { Form } from 'semantic-ui-react'
 
 class PokemonForm extends React.Component {
+  state = {
+    name: '',
+    hp: '',
+    frontUrl: '',
+    backUrl: '',
+  }
+
+  handleOnChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault()
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        hp: this.state.hp,
+        sprites: {
+          front: this.state.frontUrl,
+          back: this.state.backUrl
+        }
+      })
+    })
+      .then(resp => resp.json())
+      .then(newPokemon => this.props.addPokemon(newPokemon))
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={() => {console.log("submitting form...")}}>
+        <Form onSubmit={this.handleOnSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid label="Name" placeholder="Name" name="name" onChange={this.handleOnChange} />
+            <Form.Input fluid label="hp" placeholder="hp" name="hp" onChange={this.handleOnChange} />
+            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" onChange={this.handleOnChange} />
+            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" onChange={this.handleOnChange} />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
